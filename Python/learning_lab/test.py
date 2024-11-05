8989f47d2224286798b6ab6824a331f0dfa1d5ac
Nayil-Nord <okbnord@gmail.com>
2024-11-05 16:51:47 +0300

    Обновление линтера. Конвертация ipynb в py и все это проверка  линтеров.

---
+++ Python/learning_lab/test.py
@@ -0,0 +1,11 @@
+"""Демонстрация работы с map и распаковкой значений."""
+
+angle_a: int
+angle_b: int
+angle_c: int
+
+angle_a, angle_b, angle_c = map(int, input().split())
+
+print(angle_a)
+print(angle_b)
+print(angle_c)
